38d70bfad50bec5a8716d2f65e2397ff2fa780d0
Kieran Thakkar <kieranthakkar@gmail.com>
2023-09-28 23:04:46 +0100

    Probability Calculator

--- Probability Calculator/prob_calculator.py
+++ Probability Calculator/prob_calculator.py
@@ -3,6 +3,40 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+    def __init__(self,**kwargs) -> None:
+        self.contents = []
+        for key,value in kwargs.items():
+            i = 0
+            while i < value:
+                self.contents.append(key)
+                i += 1
+    
+    def draw(self, n: int) -> list:
+        if n > len(self.contents):
+            return self.contents
+        string = []
+        i = 0
+        while i < n:
+            popIndex = random.randint(0,len(self.contents)-1)
+            popped = self.contents[popIndex]
+            string.append(popped)
+            self.contents.pop(popIndex)
+            i += 1
+        return string
 
 
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments) -> float:
+    M = 0
+    for i in range(num_experiments):
+        original = copy.deepcopy(hat)
+        excpected = copy.deepcopy(expected_balls)
+        obtained = original.draw(num_balls_drawn)
+
+        for ball in obtained:
+            if ball in excpected:
+                excpected[ball] -= 1
+        
+        if all(count <= 0 for count in excpected.values()):
+            M += 1
+    
+    return M/num_experiments
